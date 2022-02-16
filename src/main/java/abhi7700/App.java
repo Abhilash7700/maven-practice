c19e4232715e3daae9f6582a7389cb6e28922ba4
root <root@ip-172-31-58-223.ec2.internal>
2022-02-16 12:37:21 +0000

    practice

---
+++ src/main/java/abhi7700/App.java
@@ -0,0 +1,13 @@
+package abhi7700;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
